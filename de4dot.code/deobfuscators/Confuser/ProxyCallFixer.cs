2ff7567861256697386648c4e3505729b9a746d0
Andrii Kurdiumov <kant2002@gmail.com>
2025-09-21 10:28:54 +0500

    Fix build when .NET 10 installed

--- de4dot.code/deobfuscators/Confuser/ProxyCallFixer.cs
+++ de4dot.code/deobfuscators/Confuser/ProxyCallFixer.cs
@@ -115,10 +115,10 @@ namespace de4dot.code.deobfuscators.Confuser {
 				var fields = new List<FieldDef>(fieldToMethods.GetKeys());
 				var type = DotNetUtils.GetModuleType(module);
 				if (fields.Count > 0 && type != null) {
-					foreach (var field in type.Fields) {
-						var fieldType = field.FieldType.TryGetTypeDef();
+					foreach (var fieldInfo in type.Fields) {
+						var fieldType = fieldInfo.FieldType.TryGetTypeDef();
 						if (fieldType != null && delegateTypesDict.ContainsKey(fieldType))
-							fields.Add(field);
+							fields.Add(fieldInfo);
 					}
 				}
 				return fields;
