062f6adab32fa1ce245d549a61575d2e11a7bc53
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-27 14:15:05 +0200

    Update to .NET 10 (#19)

--- build.ps1
+++ build.ps1
@@ -4,6 +4,6 @@ dotnet build -c Release -f net48 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 Remove-Item Release\net48\*.pdb, Release\net48\*.xml, Release\net48\Test.Rename.*
 
-dotnet publish -c Release -f net8.0 -o publish-net8.0 de4dot
+dotnet publish -c Release -f net10.0 -o publish-net10.0 de4dot
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item publish-net8.0\*.pdb, publish-net8.0\*.xml
+Remove-Item publish-net10.0\*.pdb, publish-net10.0\*.xml
